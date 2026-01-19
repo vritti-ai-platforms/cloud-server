569b6d5444ee12302b7ea4938dcd4cfac9695b84
Shashank Raju <shashank@vrittiai.com>
2026-01-19 17:53:00 +0530

    refactor: reorganize export statements for clarity in schema index

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,12 +1,12 @@
 // Export schema
-
+/** biome-ignore-all assist/source/organizeImports: <relations depends on tables above relation export> */
 export * from './auth';
 export * from './cloud-schema';
 // Export all enums
 export * from './enums';
-// Export relations last (depends on tables above)
-export * from './relations';
 // Export all tables
 export * from './tenant';
 export * from './user';
 export * from './verification';
+// Export relations last (depends on tables above)
+export * from './relations';
